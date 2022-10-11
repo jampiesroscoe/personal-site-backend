d014601c9bea882a0069feea0e9398ecae78a8e6
Roscoe Jampies <roscoe.jampies@standardbank.co.za>
2022-10-11 14:06:54 +0200

    clean up

--- src/curriculum-vitae/dto/update-curriculum-vitae.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCurriculumVitaeDto } from './create-curriculum-vitae.dto';
-
-export class UpdateCurriculumVitaeDto extends PartialType(
-  CreateCurriculumVitaeDto,
-) {}
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.enableCors({
-    origin: [
-      'http://localhost:4200/',
-    ],
+    origin: [],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 200,
