664095fbfbbe64a71547619d365046ba5a9974c5
Roscoe Jampies <roscoe.jampies@standardbank.co.za>
2022-10-11 17:29:02 +0200

    fixed cors

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.enableCors({
-    origin: [],
+    origin: ['https://roscoejampies1-khqzv.ondigitalocean.app/spacefun','https://roscoejampies1-khqzv.ondigitalocean.app','https://roscoejampies1-khqzv.ondigitalocean.app/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 200,
     credentials: true,
     allowedHeaders:
-      'Origin,X-Requested-With,Content-Type,Accept,Authorization,authorization,X-Forwarded-for',
+      'Origin,X-Requested-With,Content-Type,Accept,Authorization,authorization,X-Forwarded-for,strict-origin-when-cross-origin',
   })
   await app.listen(3000);
 }
